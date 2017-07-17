17edd1295b38baf1be623376ce8be7f43a18f7f1
Jakob Schönicke <jakob.schoenicke@arcor.de>
2017-07-17 16:25:11 +0200

    Created GameActivity and the connection to

--- app/src/main/java/com/starproductions/starmotion/starmotion/GameActivity.java
+++ app/src/main/java/com/starproductions/starmotion/starmotion/GameActivity.java
@@ -1,30 +1,16 @@
 package com.starproductions.starmotion.starmotion;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class GameActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_game);
         onWindowFocusChanged(true);
-        initButtons();
-    }
-
-    private void initButtons() {
-        Button startButton = findViewById(R.id.activity_main_button_start);
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, GameActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
     @Override
