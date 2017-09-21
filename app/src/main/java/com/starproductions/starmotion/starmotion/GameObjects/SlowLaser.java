41384f0a517ac6d59665fa933ee33751ed02f091
NiklasWoiton <Niklas.Woiton@stud.uni-regensburg.de>
2017-09-21 21:19:09 +0200

    Rebalanced Lasers and added SlowLaser

---
+++ app/src/main/java/com/starproductions/starmotion/starmotion/GameObjects/SlowLaser.java
@@ -0,0 +1,27 @@
+package com.starproductions.starmotion.starmotion.GameObjects;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import com.starproductions.starmotion.starmotion.GameConstants;
+import com.starproductions.starmotion.starmotion.GameEngine;
+import com.starproductions.starmotion.starmotion.R;
+
+/**
+ * Created by Admin on 21.09.2017.
+ */
+
+class SlowLaser extends Laser {
+
+    SlowLaser(GameEngine gameEngine, double posX, double posY, double speedX, double speedY){
+        super(gameEngine, posX, posY, speedX, speedY);
+    }
+
+    @Override
+    protected void setAsset() {
+        Bitmap srcAsset = BitmapFactory.decodeResource(gameEngine.getResources() , R.drawable.bullet_violet);
+        int newWidth = (int) (GameConstants.SIZE.x * GameConstants.LASER_SCALE_FACTOR_STRAIGHT);
+        int newHeight = (int) ((double) srcAsset.getHeight() * ((double) newWidth / (double) srcAsset.getWidth()));
+        asset = Bitmap.createScaledBitmap(srcAsset, newWidth, newHeight, true);
+    }
+}
